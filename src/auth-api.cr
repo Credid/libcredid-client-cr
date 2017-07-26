dc2bdfc2e99421277858842f7d04683cac14f992
Arthur Poulet <arthur.poulet@mailoo.org>
2017-07-26 21:38:57 +0100

    Add ssl support

--- src/auth-api.cr
+++ src/auth-api.cr
@@ -1,28 +1,45 @@
 require "./auth-api/*"
 require "socket"
+require "openssl"
 
 # TODO: SSL
 class Auth::Api
   @socket : TCPSocket
   @ip : String
   @port : UInt16
+  @ssl : Bool
+  @ssl_socket : OpenSSL::SSL::Socket::Client?
+  @verbosity : Bool
   property username : String
   property password : String
   getter authenticated : Bool
   getter last_result : String
 
-  def initialize(@ip = "127.0.0.1", @port = 8999_u16, @username = "root", @password = "toor", verbose = false)
+  def initialize(@ip = "127.0.0.1", @port = 8999_u16, @username = "root", @password = "toor", @ssl = false, @verbosity = false)
     @socket = TCPSocket.new(@ip, @port)
-    STDOUT.puts "Connected to #{@ip}:#{@port}@#{@username}:******" if verbose
+    @ssl_socket = nil
+    STDOUT.puts "Connected to #{@ip}:#{@port}@#{@username}:******" if @verbosity
+    if @ssl
+      ssl_context = OpenSSL::SSL::Context::Client.insecure
+      # TODO. well, it should only be used for testing
+      ssl_context.verify_mode = OpenSSL::SSL::VerifyMode::NONE
+      @ssl_socket = OpenSSL::SSL::Socket::Client.new(@socket, ssl_context)
+      STDOUT.puts "Secure connection established" if @verbosity
+    end
     @authenticated = false
     @last_result = ""
   end
 
-  private def gets(verbosity = false)
-    result = @socket.gets
+  def socket
+    @ssl_socket || @socket
+  end
+
+  private def gets
+    result = socket.gets
+    STDOUT.puts "< #{result.inspect}" if @verbosity
     if result.nil?
-      @socket.close
-      STDOUT.puts "End of connection" if verbosity
+      socket.close
+      STDOUT.puts "End of connection" if @verbosity
     else
       @last_result = result
     end
@@ -30,7 +47,9 @@ class Auth::Api
   end
 
   private def puts(data)
-    @socket.puts data
+    socket.puts data
+    socket.flush
+    STDOUT.puts "> #{data.inspect}" if @verbosity
   end
 
   SUCCESS = "success"
